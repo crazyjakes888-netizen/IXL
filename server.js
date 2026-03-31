44158ed2c707cef40275832b668589d838023a5d
JJweb <crazyjakes888@gmail.com>
2026-03-31 16:34:58 -0500

    Fix server binding to 0.0.0.0 for Render

--- server.js
+++ server.js
@@ -72,6 +72,6 @@ function getLeaderboard() {
 }
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Cookie Chat running at http://localhost:${PORT}`);
 });
